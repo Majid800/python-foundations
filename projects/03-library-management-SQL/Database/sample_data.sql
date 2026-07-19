e35088d2716b503129f2e99ffff0262fa27848d7
Majid800 <majid887799@outlook.com>
2026-07-19 18:48:54 +0100

    Add PostgreSQL database schema and sample data scripts

---
+++ projects/03-library-management-SQL/Database/sample_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO books
+(title, author, genre, year, available)
+
+VALUES
+
+('Harry Potter and the Philosopher''s Stone', 'J.K. Rowling', 'Fantasy', 1997, TRUE),
+
+('The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 1937, FALSE),
+
+('1984', 'George Orwell', 'Dystopian', 1949, TRUE),
+
+('Animal Farm', 'George Orwell', 'Political Satire', 1945, FALSE),
+
+('Dune', 'Frank Herbert', 'Science Fiction', 1965, TRUE),
+
+('The Hunger Games', 'Suzanne Collins', 'Science Fiction', 2008, TRUE),
+
+('Dracula', 'Bram Stoker', 'Horror', 1897, FALSE),
+
+('It', 'Stephen King', 'Horror', 1986, TRUE),
+
+('The Da Vinci Code', 'Dan Brown', 'Thriller', 2003, TRUE),
+
+('The Great Gatsby', 'F. Scott Fitzgerald', 'Classic', 1925, TRUE);
