7a735cb1caaaf3096d27c86d0f75e631b8656d47
Majid800 <majid887799@outlook.com>
2026-07-21 19:32:47 +0100

    Move completed library methods from test module into library module

--- projects/04-library-management-OOP/library.py
+++ projects/04-library-management-OOP/library.py
@@ -5,7 +5,7 @@ from validations import user_input, get_int_value, get_confirmation
 class library:
     def __init__(self):
         self.books = []
-
+    
     def add_book(self):
         title = user_input("Please enter Title: (press X to cancel): ")
         if title is None:
@@ -36,6 +36,149 @@ class library:
         self.books.append(book)
         print("Book added successfully!")
         
+
+    def view_all_books(self):
+        for book in self.books:
+            book.display_book()
+
+    def view_available_books(self):
+        print("\n--- Available Books ---")
+        for book in self.books: 
+            if book.available:
+                book.display_book()
+    
+    def borrowed_books(self):
+        print("\n--- Borrowed Books ---")
+        for book in self.books():
+            if not book.available:
+                book.display_book()
+
+    def search_by_title(self):
+        title = user_input("Please enter Title: (press X to cancel): ")
+        if title is None:
+            return
+        
+        for book in self.books:
+            if title == book.title:
+                book.display_book()
+                return
+            
+        print("Book does not exist!")
+
+    def search_by_author(self):
+        author = user_input("Please Enter Author: (press X to cancel): ")
+        if author is None:
+            return
+        
+        found = False
+        print(f"\n --- {author} ---")
+        for book in self.books:
+            if author == book.author:
+                found = True
+                book.display_book()
+                return
+        if not found:
+            print("Book does not exist!")
+
+    def search_by_genre(self):
+        genre = user_input("Please Enter Genre: (press X to cancel): ")
+        if genre is None:
+            return
+        
+        print(f"\n --- {genre} ---")
+        found = False 
+        for book in self.books:
+            if genre == book.genre:
+                found= True
+                book.display_book()
+        if not found: 
+            print("book does not exist!")
+            
+
+    def borrow_book(self):
+        title = user_input("Please enter Title: (press X to cancel): ")
+        if title is None:
+            return
+        author = user_input("Please enter Author: (press X to cancel): ")
+        if author is None:
+            return 
+        
+        found = False 
+
+        for book in self.books:
+            if title == book.title and author == book.author:
+                found = True 
+                book.display_book()
+                confirm = get_confirmation("Do you want to borrow this book? (Y/N): ")
+                if not confirm:
+                    print("cancelling...")
+                    return 
+                
+                if book.available:
+                    book.available = False
+                    print("Book has been successfully borrowed!")
+                else:
+                    print("book has already been borrowed!")
+        if not found:
+                print("Book does not exist!")
+                    
+    def return_book(self):
+        title = user_input("Please enter Title: (press X to cancel): ")
+        if title is None:
+            return
+        author = user_input("Please enter Author: (press X to cancel): ")
+        if author is None:
+            return
+
+        found = False
+
+        for book in self.books:
+            if title == book.title and author == book.author:
+                book.display_book()
+                found = True 
+                confirm = get_confirmation("Do you want to return this book? (Y/N): ")
+                if not confirm:
+                    print("cancelling...")
+                    return  
+            
+
+                if book.available:
+                    print("Book has already been returned!")
+                else:
+                    book.available = True
+                    print("Book has been returned!")
+        if not found:
+            print("book does not exist!")
+
+
+    def delete_book(self):
+        title = user_input("Please enter Title: (press X to cancel): ")
+        if title is None:
+            return
+        author = user_input("Please enter Author: (press X to cancel): ")
+        if author is None:
+            return
+        
+        found = False 
+
+        for book in self.books():
+            if title == book.title and author == book.author:
+                found = True 
+                book.display_book()
+                confirm = get_confirmation("Do you want to delete this book? (Y/N): ")
+                if not confirm:
+                    print("cancelling...")
+                    return
+                
+                self.books.remove(book)
+                print("Book has been deleted!")
+                return 
+            
+        if not found:
+                print("Book does not exist!")
+
+        
+    
 
     
     
