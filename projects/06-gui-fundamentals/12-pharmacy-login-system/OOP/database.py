b4ac42a4b4c4dd784fa32bb4c8d210682dcf7719
Majid800 <majid887799@outlook.com>
2026-07-28 02:17:36 +0100

    git commit --amend-- --no-edit

---
+++ projects/06-gui-fundamentals/12-pharmacy-login-system/OOP/database.py
@@ -0,0 +1,35 @@
+import psycopg
+
+def connect_database():
+    connection = psycopg.connect(
+             dbname = "pharmacy.db",
+             user = "postgres",
+             password = "King1978!", 
+             host = "localhost",
+             port = "5432")
+    cursor = connection.cursor()
+     
+    return connection, cursor  
+
+def check_login(username,password):
+    connection, cursor  = connect_database()
+
+    cursor.execute("SELECT username, password" \
+    " FROM employees" \
+    " WHERE username = %s",
+     (username,))
+
+    row = cursor.fetchone()
+    if row is None:
+
+        cursor.close()
+        connection.close()
+        return "user_not_found"
+ 
+    
+    username_db, password_db = row
+    if password_db == password:
+        return "Login Successful"
+
+    else:
+        return "password does not match" 
